4768158017901403f676236f59416e9525de6795
ruberwa <ruberwa3@gmail.com>
2023-03-07 12:59:33 +0200

    Debugging Authentication failure

--- auth.js
+++ auth.js
@@ -1,48 +1,47 @@
-const db = require("./app/models");
+const db = require('./app/models');
 const User = db.users;
-
-const passport = require('passport')
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
-const jwt = require('jsonwebtoken');
 
-// Configure passport to use the Google strategy
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/google/callback"
+    callbackURL: 'http://localhost:5000/google/callback'
   },
-
   function(accessToken, refreshToken, profile, done) {
-    // Check if the user already exists in the database
     User.findOne({ where: { email: profile.email } })
       .then(user => {
-        // If the user already exists, return the user object
         if (user) {
           return done(null, user);
+        } else {
+          const newUser = {
+            names: profile.displayName,
+            email: profile.email,
+          };
+          User.create(newUser)
+            .then(createdUser => {
+              return done(null, createdUser);
+            })
+            .catch(err => {
+              return done(err, null);
+            });
         }
-        // If the user does not exist, create a new user object and save it to the database
-        const newUser = {
-          names: profile.displayName,
-          email: profile.email,
-        };
-        User.create(newUser)
-          .then(createdUser => {
-            return done(null, createdUser);
-          })
-          .catch(err => {
-            return done(err, null);
-          });
       })
       .catch(err => {
         return done(err, null);
       });
   }));
 
-// Serialize and deserialize user
-passport.serializeUser(function(user,done){
-    done(null, user)
-})
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
-passport.deserializeUser(function(user,done){
-    done(null, user)
-})
+passport.deserializeUser(function(id, done) {
+  User.findByPk(id)
+    .then(user => {
+      done(null, user);
+    })
+    .catch(err => {
+      done(err, null);
+    });
+});
--- server.js
+++ server.js
@@ -1,94 +1,58 @@
 require('dotenv').config();
-const session = require('express-session')
-const express = require("express");
-const passport = require('passport')
-const jwt = require('jsonwebtoken');
-const cors = require("cors");
-require('./auth')
-
-const JWT_SECRET = process.env.JWT_SECRET;
+const session = require('express-session');
+const express = require('express');
+const passport = require('passport');
+const db = require('./app/models');
+require('./auth');
 
 function isLoggedIn(req, res, next) {
-  console.log('req:', req);
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.sendStatus(401);
+  req.user ? next() : res.sendStatus(401);
 }
 
-
-
 const app = express();
-app.use(session({secret: process.env.SECRET_KEY}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(session({ secret: process.env.SECRET_KEY }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-
-// parse requests of content-type - application/json
 app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-const db = require("./app/models");
 db.sequelize.sync()
   .then(() => {
-    console.log("Synced db.");
+    console.log('Synced db.');
   })
   .catch((err) => {
-    console.log("Failed to sync db: " + err.message);
+    console.log('Failed to sync db: ' + err.message);
   });
 
-//USER ROUTES
-require("./app/routes/user.routes")(app);
+require('./app/routes/user.routes')(app);
 
-// GOOGLE AUTHENTICATION
-app.get("/", (req, res) => {
-  res.send('<a href="/auth/google">Authenticate with Google</a>')
+// Google Authentication
+app.get('/', (req, res) => {
+  res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
 
 app.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
-  if (req.user) {
-    const token = jwt.sign({ userId: req.user.id }, JWT_SECRET);
-    console.log('Token:', token);
-    res.cookie('token', token);
-    res.redirect('/protected');
-  } else {
-    res.redirect('/auth/failure');
-  }
+  res.redirect('/protected');
 });
-
 
 app.get('/protected', isLoggedIn, (req, res) => {
-  res.send(`Hello ${req.user.displayName}`);
+  res.send(`Hello ${req.user.names}`);
 });
 
-app.get('/auth/failure',(req,res)=>{
-  res.send('Something went wrong')
-})
-
-app.get('/logout',(req,res)=>{
+app.get('/logout', (req, res) => {
   req.logout();
   req.session.destroy();
-  res.send('Goodbye')
-})
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.send('Goodbye');
 });
 
+app.get('/auth/failure', (req, res) => {
+  res.send('Something went wrong');
+});
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
