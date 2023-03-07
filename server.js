61463bed7a4daee74b41bb0c7b64ef0a45b26055
ruberwa <ruberwa3@gmail.com>
2023-03-07 13:13:52 +0200

    Debuging authentication failure

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 require('dotenv').config();
-<<<<<<< HEAD
 const session = require('express-session');
 const express = require('express');
 const passport = require('passport');
 const db = require('./app/models');
 require('./auth');
-=======
-const session = require('express-session')
-const express = require("express");
-const passport = require('passport')
-const jwt = require('jsonwebtoken');
-require('./auth')
-
-const JWT_SECRET = process.env.JWT_SECRET;
->>>>>>> a8c02c24e3c476387e56971dbceb8c54f4e7e550
 
 function isLoggedIn(req, res, next) {
   req.user ? next() : res.sendStatus(401);
@@ -24,13 +14,6 @@ app.use(session({ secret: process.env.SECRET_KEY }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-<<<<<<< HEAD
-=======
-
-
-
-// parse requests of content-type - application/json
->>>>>>> a8c02c24e3c476387e56971dbceb8c54f4e7e550
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
